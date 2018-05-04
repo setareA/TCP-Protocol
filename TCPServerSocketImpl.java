d7e7ece34956f9a6c0828031fa326c8572612d1a
Negar Mirgati <negar.nbj96@yahoo.com>
2018-05-04 22:25:10 +0430

    TCPServerSocketImpl accept implemented, not completed

--- TCPServerSocketImpl.java
+++ TCPServerSocketImpl.java
@@ -1,16 +1,74 @@
+import java.net.*;
+import java.io.*;
 
 public class TCPServerSocketImpl extends TCPServerSocket {
+
+	public EnhancedDatagramSocket socket;
+	private static int ACK_NUM = 0;
+	private static int SEQ_NUM = 0;
+	private static int PORT;
+	private static State STATE = State.NONE;
+	private static boolean servingClient = false;  //Blocking 
+
     public TCPServerSocketImpl(int port) throws Exception {
         super(port);
+        this.PORT = port;
+        this.ACK_NUM = 0;
+        this.socket = new EnhancedDatagramSocket(this.PORT);
+        this.log("TCP: New socket created.");
     }
 
     @Override
     public TCPSocket accept() throws Exception {
-        throw new RuntimeException("Not implemented!");
+        System.out.println("Accept");
+        byte[] receivedData= new byte[1024];
+        while(!servingClient){
+	        DatagramPacket receivedPacket = new DatagramPacket(receivedData, receivedData.length);
+	        this.socket.receive(receivedPacket);
+	        String receivedString = new String(receivedPacket.getData());
+	        InetAddress client_ip = receivedPacket.getAddress();
+	        int client_port = receivedPacket.getPort();
+	        System.out.println("khar" + client_port);
+	        System.out.println("TCP Received : " + receivedData);
+	        String[] receivedSplited = receivedString.split("\\s+");
+	        ///////
+	        // TODO int receivedAckNum = ???
+	        if(this.STATE == State.NONE){
+	        	if(receivedSplited[0].equals("SYN")){ //first packet must be a SYN packet
+								this.log("3-way handshaking 1/3");
+								String ACK_NUM_Str = Integer.toString(this.ACK_NUM);
+								String SEQ_NUM_Str = Integer.toString(this.SEQ_NUM);
+								String sendDataStr = "SYN-ACK" + " " + SEQ_NUM_Str + " "+ ACK_NUM_Str;
+								byte[] sendDataBytes = new byte[1024];
+								sendDataBytes = sendDataStr.getBytes();
+
+								DatagramPacket sendPacket = new DatagramPacket(sendDataBytes, sendDataBytes.length, client_ip, client_port);
+								this.socket.send(sendPacket);
+
+								this.STATE = State.SYN_RECV;
+								this.log("State Change : SYN_RECV");
+							}
+	        }
+	        else if(this.STATE == State.SYN_RECV){
+
+	        	if(receivedSplited.equals("ACK") && (receivedAckNum == (this.SEQ_NUM + 1))){
+	        		this.STATE = State.ESTABLISHED;
+	        		servingClient = true;
+	        		this.log("State Change : ESTABLISHED");
+	       	    }
+	       	}
+	    }
+	       	
+        TCPSocketImpl tcpSocket = new TCPSocketImpl("127.0.0.1",1234);
+        return tcpSocket;
+    }
+
+    public void log(String log_data){
+    	System.out.println(log_data);
     }
 
     @Override
     public void close() throws Exception {
-        throw new RuntimeException("Not implemented!");
+         this.socket.close();
     }
 }
