f02b6b293856bab7ba9b6d2418a3c3df4f765e9f
Negar Mirgati <negar.nbj96@yahoo.com>
2018-05-04 23:43:18 +0430

    sener receiver port changed

--- Sender.java
+++ Sender.java
@@ -3,7 +3,7 @@ import java.net.*;
 
 public class Sender {
     public static void main(String[] args) throws Exception {
-        TCPSocket tcpSocket = new TCPSocketImpl("127.0.0.1", 12345);
+        TCPSocket tcpSocket = new TCPSocketImpl("127.0.0.1", 8088);
         tcpSocket.send("sending.mp3");
         tcpSocket.close();
         tcpSocket.saveCongestionWindowPlot();
