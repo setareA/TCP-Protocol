ba0115a605984b3aeac981740f11cad0a0a3c77c
Negar Mirgati <negar.nbj96@yahoo.com>
2018-05-04 22:37:51 +0430

    TCPSocketImpl some variables added

--- TCPSocketImpl.java
+++ TCPSocketImpl.java
@@ -1,8 +1,27 @@
 import java.util.Random;
+import java.io.*;
 
 public class TCPSocketImpl extends TCPSocket {
+    public EnhancedDatagramSocket socket;
+    private String IP;
+    private int PORT;
+    private int SEQ_NO;
+    private int ACK_NO;
+
     public TCPSocketImpl(String ip, int port) throws Exception {
         super(ip, port);
+        this.log("Constructor Called");
+        System.out.println(port);
+        this.IP = ip;
+        this.PORT = port;
+        this.SEQ_NUM = 0;
+        this.ACK_NUM = 0;
+        this.socket= new EnhancedDatagramSocket(this.PORT);
+
+    }
+
+    public void log(String log_Data){
+        System.out.println("TCPSocketImpl : " + log_Data);
     }
 
     @Override
@@ -17,7 +36,8 @@ public class TCPSocketImpl extends TCPSocket {
 
     @Override
     public void close() throws Exception {
-        throw new RuntimeException("Not implemented!");
+        this.socket.close();
+        //throw new RuntimeException("Not implemented!");
     }
 
     @Override
